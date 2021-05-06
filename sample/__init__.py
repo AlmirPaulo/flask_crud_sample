3b3247904e73dc5b715eae9624f92a371d3bee34
AlmirPaulo <ap.freelas@gmail.com>
2021-05-06 18:49:22 -0300

    js form validation, loggings implementation

--- sample/__init__.py
+++ sample/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os, logging
                                 #change this after deploy
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s:%(module)s:%(levelname)s:%(message)s')
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
+
 
 #configurations
 app.config['SECRET_KEY'] = 'Some really relevant sequency of characters, much better than this one.'
@@ -14,5 +16,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
 
 if not os.path.exists('sample/data.db'):
-    db.create_all()
-    logging.info('Database created!')
+    try:
+        db.create_all()
+        logger.info('Database created!')
+    except:
+        logger.critical('Database could NOT be created!')
--- sample/static/validation.js
+++ sample/static/validation.js
@@ -1,9 +1,9 @@
 // variables
-let username = document.getElementById('username');
-let password = document.getElementById('password');
-let conf = document.getElementById('conf');
+let username = document.getElementById('username').innerText;
+let password = document.getElementById('password').innerText;
+let conf = document.getElementById('conf').innerText;
 let btn = document.querySelector('.btn');
-//let err = document.querySelector('.err');
+let err = document.querySelector('.err');
 
 //equal passwords
 function equal_passwords(){
@@ -14,24 +14,58 @@ function equal_passwords(){
 	}
 };
 
-// minimal lenght
-function min_len(){
+// minimal lenght password
+function pass_min_len(){
 	if (password.lenght < 8){
-	return false
-	}else if(password.lenght > 12){
 		return false
-	}else if(username.lenght < 3){
-		return false
-	}else if(username.lenght > 10){
-		return false
-	} else{
+	}else{
 		return true
 	}
 };
 
-//validation
-btn.addEventListener('click', function(ev){
-	if (min_len() === false || equal_passwords() === false){
-	ev.preventDefault()	
+// max length password
+function pass_max_len(){
+	if(password.lenght > 12){
+		return false
+		}else{
+		return true
+		}};
+		
+// min length user
+function user_min_len(){
+	if(username.lenght > 3){
+		return true
+	}else{
+		return false
+	}
+};
+
+//max lentgh user
+function user_max_len(){
+	if(username.lenght < 10){
+		return true
+}else{
+	return false
 }
-});
+};
+
+//validation -- Need a debugging
+btn.addEventListener('click', function(ev){
+	if (user_min_len() === false){
+		err.innerText = "Username is too short"
+		ev.preventDefault()
+}else if(user_max_len() === false){
+		err.innerText = "Username is too long"
+		ev.preventDefault()
+}else if (equal_passwords() === false){
+		err.innerText = 'The passwords do not match'
+		ev.preventDefault()
+		console.log('The passwords do not match')	
+}else if (pass_min_len() === false) {
+		err.innerText = 'Password is too short'
+		ev.preventDefault()	
+}else if (pass_max_len() === false) {
+		err.innerText = 'Password is too long'
+		ev.preventDefault()	
+		
+}});
